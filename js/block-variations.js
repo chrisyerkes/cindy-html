42cb235bb04b3d7b96dc8dbc8ad530983e8c32d2
Chris Yerkes <hello@mopsdigital.com>
2023-05-24 23:32:01 +0000

    Adding a couple more block variations for group block.

--- js/block-variations.js
+++ js/block-variations.js
@@ -3,7 +3,22 @@ wp.blocks.registerBlockVariation('core/group', {
 	title: 'Fluid Container',
 	isDefault: true,
 	attributes: {
-		align: 'full',
-		className: 'is-style-page-content',
+		className: 'container-fluid is-style-page-content',
+	},
+});
+
+wp.blocks.registerBlockVariation('core/group', {
+	name: 'container',
+	title: 'Container',
+	attributes: {
+		className: 'container is-style-page-content',
+	},
+});
+
+wp.blocks.registerBlockVariation('core/group', {
+	name: 'row',
+	title: 'Row',
+	attributes: {
+		className: 'row',
 	},
 });
