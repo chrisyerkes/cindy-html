7d27fa6d0e81048b41c42d196e734888a49a4916
Chris Yerkes <hello@mopsdigital.com>
2023-05-31 00:13:02 +0000

    Fixing page content wrapper to allow for exterior to div wave art.

--- patterns/page-content-wrapper.php
+++ patterns/page-content-wrapper.php
@@ -11,8 +11,11 @@
 ?>
 
 <!-- wp:group {"tagName":"section","className":"is-style-cindylau-page-content","layout":{"type":"constrained"}} -->
-<section class="wp-block-group is-style-cindylau-page-content"><!-- wp:group {"className":"container","layout":{"type":"constrained"}} -->
-	<div class="wp-block-group container"></div>
+<section class="wp-block-group is-style-cindylau-page-content"><!-- wp:group {"className":"full-width-content-wrapper","layout":{"type":"default"},"anchor":""} -->
+	<div class="wp-block-group full-width-content-wrapper"><!-- wp:group {"className":"container","layout":{"type":"constrained"}} -->
+		<div class="wp-block-group container"></div>
+		<!-- /wp:group -->
+	</div>
 	<!-- /wp:group -->
 </section>
 <!-- /wp:group -->
